48695c68df7705d439a86ac1265e4288dcd608a7
Jyoti Prakash Sethy <92805753+jyoti-prakash-25@users.noreply.github.com>
2022-07-10 08:25:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-new Vue ({
-	el: '#taskList',
-	data: {
-		title: 'to do list',
-		tasks: [
-			{ name: 'Start a new pen' },
-			{ name: 'Read a book' },
-			{ name: 'Meeting with team' }
-		]
-	},
-	methods: {
-		newItem: function() {
-			if (!this.tasks.name) {
-				return
-			}
-			this.tasks.push ( {
-				name: this.tasks.name,
-				del: ''
-			});
-      this.tasks.name = "";
-		},
-		delItem: function (task) {
-			this.tasks.splice(this.tasks.indexOf(task), 1)
-		}
-	}
-})
+const addForm = document.querySelector("form.add");
+const ul = document.querySelector("ul.todos");
+const searchFormInput = document.querySelector("form.search input");
+
+//ADD NEW TODO
+const handleAddItem = (inputValue) => {
+  const html = `
+   <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${inputValue}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+  ul.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = addForm.add.value;
+  if (inputValue.length) handleAddItem(inputValue);
+  addForm.add.value = "";
+});
+
+//REMOVE TODO
+ul.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+//SEARCH INPUT: PREVENT DEFAULT ACTION - LITTLE BUG I FOUND IN THE COURSE PROJECT
+searchFormInput.parentElement.addEventListener("submit", (e) =>
+  e.preventDefault()
+);
+
+//SEARCH AND FILTER TODOS
+const filterItems = (value) => {
+  Array.from(ul.children).forEach((li) => {
+    if (!li.textContent.toLowerCase().includes(value)) li.classList.add("filtred");
+    else li.classList.remove("filtred");
+  });
+};
+
+searchFormInput.addEventListener("keyup", (e) => {
+  const value = searchFormInput.value.toLowerCase().trim();
+  filterItems(value);
+});
